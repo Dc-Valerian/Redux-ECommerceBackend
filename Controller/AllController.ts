4462ed5193979d8412a05a763f6c93d97b25ecca
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-27 01:01:07 +0100

    added both contoller and router together in one file

--- Controller/AllController.ts
+++ Controller/AllController.ts
@@ -1,2 +1,8 @@
 import UserModel from "../Model/User.Model";
 import ProductsModel from "../Model/Products.Model";
+import express,{Request,Response} from "express"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const router = express.Router()
+
